3e1618a7c014f53624a3b3a8fb5d8e5f6ec5356f
Edward Brennan <EdMan1022@gmail.com>
2018-10-26 16:48:23 -0400

    Attempt to load and instantiate an external class

--- src/main/groovy/utilities/baseimage.groovy
+++ src/main/groovy/utilities/baseimage.groovy
@@ -1,6 +1,15 @@
 
-def baseImage() {
-    return 'foo'
+class BaseImage {
+
+    String foo
+
+    def getFoo() {
+        return this.foo
+    }
+
+    BaseImage(bar) {
+        this.foo = bar
+    }
 }
 
 return this
